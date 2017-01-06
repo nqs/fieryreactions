751eae2f123b90ad5812ecc35dfa8696614a1a96
Nick <nstewart@truefit.io>
2017-01-05 20:50:18 -0500

    WIP

--- admin/src/actions/index.js
+++ admin/src/actions/index.js
@@ -1,2 +1,2 @@
-export * from './messageActions';
+export * from './boardActions';
 export * from './loginActions';
--- admin/src/utils/listeners.js
+++ admin/src/utils/listeners.js
@@ -4,42 +4,17 @@ import { bindActionCreators } from 'redux';
 import { boards } from './firebase';
 import * as Actions from '../actions';
 
-const ListenersService = (props) => {
-  messages.once('value').then((snapshot) => {
-    const list = [];
-    snapshot.forEach((childSnapshot) => {
-      list.push({ ...childSnapshot.val(), Id: childSnapshot.key });
-    });
-    props.messagesReceived(list);
-  });
+export class Listeners {
+  constructor(store) {
+    this.dispatch = store.dispatch;
+    this.store = store;
+    this.listening = false;
+  }
 
-  messages.on('child_changed', (snapshot) => {
-    props.messageChanged({ ...snapshot.val(), Id: snapshot.key });
-  });
-
-  messages.on('child_added', (snapshot) => {
-    props.messageAdded({ ...snapshot.val(), Id: snapshot.key });
-  });
-
-  messages.on('child_removed', (snapshot) => {
-    props.messageRemoved({ ...snapshot.val(), Id: snapshot.key });
-  });
-
-  return <span />;
-};
-
-
-ListenersService.propTypes = {
-  messagesReceived: PropTypes.func.isRequired,
-  messageChanged: PropTypes.func.isRequired,
-  messageAdded: PropTypes.func.isRequired,
-  messageRemoved: PropTypes.func.isRequired,
-};
-
-const mapDispatchToProps = (dispatch) => {
-  return bindActionCreators(Actions, dispatch);
-};
-export const Listeners = connect(
-  null,
-  mapDispatchToProps
-)(ListenersService);
+  startListening() {
+    if (this.listening) {
+      return;
+    }
+    this.listening = true;
+  }
+}
